962235b6511b26d6af9bb95f7b7b09ec2f1b24f6
dmytro.denysko <dmytro.denysko@pivdenny.ua>
2019-12-04 16:36:38 +0200

    add gitignore and Readme.md

---
+++ README.md
@@ -0,0 +1,11 @@
+#Stickers
+
+###Info
+### build project 
+mvn clean compile assembly:single
+
+###run application
+java -jar {project path}/target/stickers-1.0-SNAPSHOT.jar
+Wait Post request with text body on URL http://localhost:8082/print
+ 
+ 
--- src/main/java/StickersListener.java
+++ src/main/java/StickersListener.java
@@ -6,7 +6,7 @@ import com.sun.net.httpserver.HttpServer;
 public class StickersListener {
     public static void runServer() throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8082), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/print", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
